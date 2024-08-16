73e258e6a6cf0b84bbbcb2703b726deb80e8b878
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-08-16 14:31:32 +0000

    feat: add bot login

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { Client, Events, } from "discord.js";
+
+const client = new Client({ intents: [] });
+
+client.once(Events.ClientReady, readyClient => {
+  console.log(`Ready! logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN);
+
